292028b8af7c8e3f46c62d4b6708869da5bf805c
mbabaie <43761552+mbabaie@users.noreply.github.com>
2021-10-04 10:23:17 -0700

    Revert "DCC PR (Dev/Orb)"

--- src/mem/packet.hh
+++ src/mem/packet.hh
@@ -375,8 +375,6 @@ class Packet : public Printable
     */
     PacketDataPtr data;
 
-    PacketDataPtr cacheLineRead;
-
     /// The address of the request.  This address could be virtual or
     /// physical, depending on the system configuration.
     Addr addr;
@@ -411,9 +409,6 @@ class Packet : public Printable
     uint64_t htmTransactionUid;
 
   public:
-
-    PacketDataPtr getCacheLineRead() { return cacheLineRead;}
-
 
     /**
      * The extra delay from seeing the packet until the header is
@@ -855,8 +850,7 @@ class Packet : public Printable
      */
     Packet(const RequestPtr &_req, MemCmd _cmd)
         :  cmd(_cmd), id((PacketId)_req.get()), req(_req),
-           data(nullptr), cacheLineRead(nullptr), addr(0),
-           _isSecure(false), size(0),
+           data(nullptr), addr(0), _isSecure(false), size(0),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -897,7 +891,7 @@ class Packet : public Printable
      */
     Packet(const RequestPtr &_req, MemCmd _cmd, int _blkSize, PacketId _id = 0)
         :  cmd(_cmd), id(_id ? _id : (PacketId)_req.get()), req(_req),
-           data(nullptr), cacheLineRead(nullptr), addr(0), _isSecure(false),
+           data(nullptr), addr(0), _isSecure(false),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -924,7 +918,6 @@ class Packet : public Printable
     Packet(const PacketPtr pkt, bool clear_flags, bool alloc_data)
         :  cmd(pkt->cmd), id(pkt->id), req(pkt->req),
            data(nullptr),
-           cacheLineRead(nullptr),
            addr(pkt->addr), _isSecure(pkt->_isSecure), size(pkt->size),
            bytesValid(pkt->bytesValid),
            _qosValue(pkt->qosValue()),
