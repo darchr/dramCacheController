5b4d2195907b227293d54a10747065c105ab3f79
Ayaz Akram <yazakram@ucdavis.edu>
2021-10-20 17:39:41 -0700

    mem: Add an update simple.py
    Change-Id: I722afbb8c7c0bf08ea72c2220e8712c8ed5eba55

--- simple.py
+++ simple.py
@@ -1,5 +1,52 @@
 from m5.objects import *
 import m5
+import argparse
+
+args = argparse.ArgumentParser()
+
+# This scipt takes these arguments [traffic mode] [address range]
+# [rd percentage] [duration of simulation in ticks]
+# [request injection period in ticks] [device model for dram cache]
+
+# sample cmd: gem5.opt simple.py linear 4GB 70 1000000000 10000 DDR3_1600_8x8
+
+args.add_argument(
+    "traffic_mode",
+    type = str,
+    help = "Traffic type to use"
+)
+
+args.add_argument(
+    "address",
+    type=str,
+    help="End address of the range to be accessed",
+)
+
+args.add_argument(
+    "rd_prct",
+    type=int,
+    help="Read Percentage",
+)
+
+args.add_argument(
+    "duration",
+    type = int,
+    help = "Duration of simulation"
+)
+
+args.add_argument(
+    "inj_period",
+    type = int,
+    help = "Period to inject reqs"
+)
+
+args.add_argument(
+    "device",
+    type = str,
+    help = "Memory device to use as a dram cache"
+)
+
+options = args.parse_args()
 
 system = System()
 system.clk_domain = SrcClockDomain()
@@ -10,10 +57,8 @@ system.mem_mode = 'timing'
 system.generator = PyTrafficGen()
 
 system.mem_ctrl = DcacheCtrl()
-system.mem_ctrl.dram = DDR3_1600_8x8(range=AddrRange('8GB'),
-#system.mem_ctrl.dram = DDR4_2400_16x4(range=AddrRange('32GB'),
-#system.mem_ctrl.dram = HBM_1000_4H_1x128(range=AddrRange('128MiB'),
-in_addr_map=False)
+system.mem_ctrl.dram = eval(options.device)(range=AddrRange('8GB'),
+                                                in_addr_map=False)
 system.mem_ctrl.nvm = NVM_2400_1x64(range=AddrRange('8GB'))
 
 system.mem_ctrl.dram.tREFI = "200"
@@ -25,24 +70,24 @@ system.mem_ranges = [AddrRange('8GB')]
 system.generator.port = system.mem_ctrl.port
 
 def createRandomTraffic(tgen):
-    yield tgen.createRandom(100000000000,   # duration
+    yield tgen.createRandom(options.duration,   # duration
                             0,              # min_addr
-                            16700000,              # max_adr
+                            AddrRange(options.address).end,  # max_adr
                             64,             # block_size
-                            10000,          # min_period
-                            10000,          # max_period
-                            70,             # rd_perc
+                            options.inj_period,       # min_period
+                            options.inj_period,       # max_period
+                            options.rd_prct,          # rd_perc
                             0)              # data_limit
     yield tgen.createExit(0)
 
 def createLinearTraffic(tgen):
-    yield tgen.createLinear(10000000000,   # duration
+    yield tgen.createLinear(options.duration,   # duration
                             0,              # min_addr
-                            AddrRange('8GB').end,  # max_adr
+                            AddrRange(options.address).end,  # max_adr
                             64,             # block_size
-                            1000,          # min_period
-                            1000,          # max_period
-                            70,             # rd_perc
+                            options.inj_period,   # min_period
+                            options.inj_period,   # max_period
+                            options.rd_prct,       # rd_perc
                             0)              # data_limit
     yield tgen.createExit(0)
 
@@ -50,5 +95,13 @@ def createLinearTraffic(tgen):
 root = Root(full_system=False, system=system)
 
 m5.instantiate()
-system.generator.start(createRandomTraffic(system.generator))
+
+if options.traffic_mode == 'linear':
+    system.generator.start(createLinearTraffic(system.generator))
+elif options.traffic_mode == 'random':
+    system.generator.start(createRandomTraffic(system.generator))
+else:
+    print('Wrong traffic type! Exiting!')
+    exit()
+
 exit_event = m5.simulate()
