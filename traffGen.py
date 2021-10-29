54a4adf7020cc6b50b6b9d3785a163b580614c12
mbabaie <mbabaie@ucdavis.edu>
2021-10-29 14:41:32 -0700

    mem: traffic generator file
    Change-Id: Idc97a96895069db76f041591a776cf173f778a47

---
+++ traffGen.py
@@ -0,0 +1,51 @@
+from m5.objects import *
+import m5
+
+system = System()
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = "4GHz"
+system.clk_domain.voltage_domain = VoltageDomain()
+system.mem_mode = 'timing'
+
+system.generator = PyTrafficGen()
+
+system.mem_ctrl = DcacheCtrl()
+
+system.mem_ctrl.dram = DDR3_1600_8x8(range=AddrRange('16MiB'),
+                                     in_addr_map=False)
+system.mem_ctrl.nvm = NVM_2400_1x64(range=AddrRange('16GB'))
+
+system.mem_ctrl.dram.tREFI = "200"
+system.mem_ctrl.dram_cache_size = "16MiB"
+system.mem_ctrl.orb_max_size = "1024"
+system.mem_ctrl.crb_max_size = "16"
+
+system.mem_ranges = [AddrRange('16GB')]
+system.generator.port = system.mem_ctrl.port
+
+def createRandomTraffic(tgen):
+    yield tgen.createRandom(100000000000, # duration
+                            0, # min_addr
+                            AddrRange('128MiB').end, # max_adr
+                            64, # block_size
+                            1000, # min_period
+                            1000, # max_period
+                            100, # rd_perc
+                            0) # data_limit
+    yield tgen.createExit(0)
+
+def createLinearTraffic(tgen):
+    yield tgen.createLinear(100000000000, # duration
+                            0, # min_addr
+                            AddrRange('128MiB').end, # max_adr
+                            64, # block_size
+                            1000, # min_period
+                            1000, # max_period
+                            100, # rd_perc
+                            0) # data_limit
+    yield tgen.createExit(0)
+
+root = Root(full_system=False, system=system)
+m5.instantiate()
+system.generator.start(createLinearTraffic(system.generator))
+exit_event = m5.simulate()
