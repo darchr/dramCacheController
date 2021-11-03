da989a7377e7d023b63ca5aaa5571ed7e790d6ae
Ayaz Akram <yazakram@ucdavis.edu>
2021-11-02 17:10:55 -0700

    mem: DDR5 (Approx.) configuration -- in progress
    Change-Id: I57a757aa35d0bf5b03c4d3d7529dc61d1b9f0457

--- src/mem/DRAMDCInterface.py
+++ src/mem/DRAMDCInterface.py
@@ -416,6 +416,123 @@ class DDR3_2133_8x8(DDR3_1600_8x8):
     IDD6 ='20mA'
     VDD = '1.5V'
 
+# A single DDR5-2400 x64 channel (one command and address bus), with
+# timings based on a DDR4-2400 8 Gbit datasheet (Micron MT40A2G4)
+# in an 16x4 configuration.
+# Total channel capacity is 32GiB
+# 16 devices/rank * 2 ranks/channel * 1GiB/device = 32GiB/channel
+class DDR5_4800_1x16(DRAMDCInterface):
+    # size of device
+    device_size = '1GiB'
+
+    # single channel of 32bit width, will require 2-bit wide
+    # 16 devices
+    device_bus_width = 2
+
+    # DDR5 is a BL16 device
+    burst_length = 16
+
+    # Each device has a page (row buffer) size of 512 byte (1K columns x4)
+    device_rowbuffer_size = '2KB'
+
+    # 1x16 configuration, so 16 devices
+    devices_per_rank = 16
+
+    # Not sure about this
+    ranks_per_channel = 2
+
+    #
+    bank_groups_per_rank = 4
+
+    # 1Gbx16 Configuration of DDR5 has 4 bank groups,
+    # 4 banks per bank group and 16 banks
+    banks_per_rank = 16
+
+    # I think this defines the controller's read
+    # write queue size by default
+    write_buffer_size = 128
+    read_buffer_size = 64
+
+    # For 4800 MT/s
+    tCK = '0.416ns'
+
+    # 8 beats across an x64 interface translates to 4 clocks @ 1200 MHz
+    # tBURST is equivalent to the CAS-to-CAS delay (tCCD)
+    # With bank group architectures, tBURST represents the CAS-to-CAS
+    # delay for bursts to different bank groups (tCCD_S)
+    tBURST = '3.332ns'
+
+    # @2400 data rate, tCCD_L is 6 CK
+    # CAS-to-CAS delay for bursts to the same bank group
+    # tBURST is equivalent to tCCD_S; no explicit parameter required
+    # for CAS-to-CAS delay for bursts to different bank groups
+    tCCD_L = '5ns';
+
+    # DDR5 17-17-17
+    tRCD = '16ns'
+    # did not find
+    tCL = '14.16ns'
+    tRP = '16ns'
+    tRAS = '32ns'
+
+    # did not find
+    tRRD = '3.332ns'
+
+    # RRD_S (different bank group) is MAX(8nCK, 5ns)
+    tRRD_L = '4.9ns';
+
+    # tFAW for 512B page is MAX(16 CK, 13ns)
+    tXAW = '13.328ns'
+    activation_limit = 4
+    # tRFC is 350ns
+    tRFC = '350ns'
+
+    tWR = '15ns'
+
+    # Here using the average of WTR_S and WTR_L
+    tWTR = '5ns'
+
+    # Greater of 4 CK or 7.5 ns
+    tRTP = '7.5ns'
+
+    # Default same rank rd-to-wr bus turnaround to 2 CK, @1200 MHz = 1.666 ns
+    tRTW = '1.666ns'
+
+    # Default different rank bus delay to 2 CK, @1200 MHz = 1.666 ns
+    tCS = '1.666ns'
+
+    # <=85C, half for >85C
+    tREFI = '7.8us'
+
+    # active powerdown and precharge powerdown exit time
+    tXP = '6ns'
+
+    # self refresh exit time
+    # exit delay to ACT, PRE, PREALL, REF, SREF Enter, and PD Enter is:
+    # tRFC + 10ns = 340ns
+    tXS = '340ns'
+
+    # Current values from datasheet
+    IDD0 = '122mA'
+    #IDD0F
+    IDD02 = '164mA'
+    IDD2N = '92mA'
+    IDD3N = '142mA'
+    IDD3N2 = '142mA'
+    IDD4W = '479mA'
+    IDD4R = '530mA'
+    #IDD5B
+    IDD5 = '277mA'
+    #Not in the datasheet
+    IDD3P1 = '32mA'
+    IDD2P1 = '25mA'
+    #IDD6N
+    IDD6 = '102mA'
+    VDD = '1.1V'
+    #Not in the datasheet
+    VDD2 = '2.5V'
+
+
 # A single DDR4-2400 x64 channel (one command and address bus), with
 # timings based on a DDR4-2400 8 Gbit datasheet (Micron MT40A2G4)
 # in an 16x4 configuration.
