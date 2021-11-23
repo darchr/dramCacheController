739f363a35685e8bd81fa70b65977eb7225e7803
Ayaz Akram <yazakram@ucdavis.edu>
2021-11-23 09:55:50 -0800

    mem: small updates in bash scripts (need to compress the command)
    Change-Id: I0828be9d84dcde52dd355937491e5af7d4d38d5c

--- run_hbm.sh
+++ run_hbm.sh
@@ -1,55 +1,55 @@
 #hbm_12_1000_WO_Miss
 
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/1 traffGen.py HBM_1000_4H_1x128 16MiB 1 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/2 traffGen.py HBM_1000_4H_1x128 16MiB 2 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/4 traffGen.py HBM_1000_4H_1x128 16MiB 4 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/8 traffGen.py HBM_1000_4H_1x128 16MiB 8 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/16 traffGen.py HBM_1000_4H_1x128 16MiB 16 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/32 traffGen.py HBM_1000_4H_1x128 16MiB 32 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/64 traffGen.py HBM_1000_4H_1x128 16MiB 64 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/128 traffGen.py HBM_1000_4H_1x128 16MiB 128 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/256 traffGen.py HBM_1000_4H_1x128 16MiB 256 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/512 traffGen.py HBM_1000_4H_1x128 16MiB 512 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/1024 traffGen.py HBM_1000_4H_1x128 16MiB 1024 linear 1000000000000 128MiB 1000 0 NVM_300_1x64
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/1 traffGen.py hbm_1000 16MiB 1 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/2 traffGen.py hbm_1000 16MiB 2 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/4 traffGen.py hbm_1000 16MiB 4 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/8 traffGen.py hbm_1000 16MiB 8 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/16 traffGen.py hbm_1000 16MiB 16 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/32 traffGen.py hbm_1000 16MiB 32 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/64 traffGen.py hbm_1000 16MiB 64 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/128 traffGen.py hbm_1000 16MiB 128 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/256 traffGen.py hbm_1000 16MiB 256 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/512 traffGen.py hbm_1000 16MiB 512 linear 1000000000000 128MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Miss/1024 traffGen.py hbm_1000 16MiB 1024 linear 1000000000000 128MiB 1000 0 nvm_300
 
 #hbm_12_1000_WO_Hit
 
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/1 traffGen.py HBM_1000_4H_1x128    128MiB 1 linear 1000000000000    16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/2 traffGen.py HBM_1000_4H_1x128    128MiB 2 linear 1000000000000    16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/4 traffGen.py HBM_1000_4H_1x128    128MiB 4 linear 1000000000000    16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/8 traffGen.py HBM_1000_4H_1x128    128MiB 8 linear 1000000000000    16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/16 traffGen.py HBM_1000_4H_1x128   128MiB 16 linear 1000000000000   16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/32 traffGen.py HBM_1000_4H_1x128   128MiB 32 linear 1000000000000   16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/64 traffGen.py HBM_1000_4H_1x128   128MiB 64 linear 1000000000000   16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/128 traffGen.py HBM_1000_4H_1x128  128MiB 128 linear 1000000000000  16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/256 traffGen.py HBM_1000_4H_1x128  128MiB 256 linear 1000000000000  16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/512 traffGen.py HBM_1000_4H_1x128  128MiB 512 linear 1000000000000  16MiB 1000 0 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/1024 traffGen.py HBM_1000_4H_1x128 128MiB 1024 linear 1000000000000 16MiB 1000 0 NVM_300_1x64
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/1 traffGen.py hbm_1000    128MiB 1 linear 1000000000000    16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/2 traffGen.py hbm_1000    128MiB 2 linear 1000000000000    16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/4 traffGen.py hbm_1000    128MiB 4 linear 1000000000000    16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/8 traffGen.py hbm_1000    128MiB 8 linear 1000000000000    16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/16 traffGen.py hbm_1000   128MiB 16 linear 1000000000000   16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/32 traffGen.py hbm_1000   128MiB 32 linear 1000000000000   16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/64 traffGen.py hbm_1000   128MiB 64 linear 1000000000000   16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/128 traffGen.py hbm_1000  128MiB 128 linear 1000000000000  16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/256 traffGen.py hbm_1000  128MiB 256 linear 1000000000000  16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/512 traffGen.py hbm_1000  128MiB 512 linear 1000000000000  16MiB 1000 0 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_WO_Hit/1024 traffGen.py hbm_1000 128MiB 1024 linear 1000000000000 16MiB 1000 0 nvm_300
 
 #hbm_12_1000_RO_Hit
 
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/1 traffGen.py HBM_1000_4H_1x128    128MiB 1 linear 1000000000000    16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/2 traffGen.py HBM_1000_4H_1x128    128MiB 2 linear 1000000000000    16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/4 traffGen.py HBM_1000_4H_1x128    128MiB 4 linear 1000000000000    16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/8 traffGen.py HBM_1000_4H_1x128    128MiB 8 linear 1000000000000    16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/16 traffGen.py HBM_1000_4H_1x128   128MiB 16 linear 1000000000000   16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/32 traffGen.py HBM_1000_4H_1x128   128MiB 32 linear 1000000000000   16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/64 traffGen.py HBM_1000_4H_1x128   128MiB 64 linear 1000000000000   16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/128 traffGen.py HBM_1000_4H_1x128  128MiB 128 linear 1000000000000  16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/256 traffGen.py HBM_1000_4H_1x128  128MiB 256 linear 1000000000000  16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/512 traffGen.py HBM_1000_4H_1x128  128MiB 512 linear 1000000000000  16MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/1024 traffGen.py HBM_1000_4H_1x128 128MiB 1024 linear 1000000000000 16MiB 1000 100 NVM_300_1x64
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/1 traffGen.py hbm_1000    128MiB 1 linear 1000000000000    16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/2 traffGen.py hbm_1000    128MiB 2 linear 1000000000000    16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/4 traffGen.py hbm_1000    128MiB 4 linear 1000000000000    16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/8 traffGen.py hbm_1000    128MiB 8 linear 1000000000000    16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/16 traffGen.py hbm_1000   128MiB 16 linear 1000000000000   16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/32 traffGen.py hbm_1000   128MiB 32 linear 1000000000000   16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/64 traffGen.py hbm_1000   128MiB 64 linear 1000000000000   16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/128 traffGen.py hbm_1000  128MiB 128 linear 1000000000000  16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/256 traffGen.py hbm_1000  128MiB 256 linear 1000000000000  16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/512 traffGen.py hbm_1000  128MiB 512 linear 1000000000000  16MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Hit/1024 traffGen.py hbm_1000 128MiB 1024 linear 1000000000000 16MiB 1000 100 nvm_300
 
 #hbm_12_1000_RO_Miss
 
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/1 traffGen.py HBM_1000_4H_1x128 16MiB 1 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/2 traffGen.py HBM_1000_4H_1x128 16MiB 2 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/4 traffGen.py HBM_1000_4H_1x128 16MiB 4 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/8 traffGen.py HBM_1000_4H_1x128 16MiB 8 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/16 traffGen.py HBM_1000_4H_1x128 16MiB 16 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/32 traffGen.py HBM_1000_4H_1x128 16MiB 32 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/64 traffGen.py HBM_1000_4H_1x128 16MiB 64 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/128 traffGen.py HBM_1000_4H_1x128 16MiB 128 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/256 traffGen.py HBM_1000_4H_1x128 16MiB 256 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/512 traffGen.py HBM_1000_4H_1x128 16MiB 512 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
-build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/1024 traffGen.py HBM_1000_4H_1x128 16MiB 1024 linear 1000000000000 128MiB 1000 100 NVM_300_1x64
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/1 traffGen.py hbm_1000 16MiB 1 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/2 traffGen.py hbm_1000 16MiB 2 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/4 traffGen.py hbm_1000 16MiB 4 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/8 traffGen.py hbm_1000 16MiB 8 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/16 traffGen.py hbm_1000 16MiB 16 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/32 traffGen.py hbm_1000 16MiB 32 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/64 traffGen.py hbm_1000 16MiB 64 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/128 traffGen.py hbm_1000 16MiB 128 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/256 traffGen.py hbm_1000 16MiB 256 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/512 traffGen.py hbm_1000 16MiB 512 linear 1000000000000 128MiB 1000 100 nvm_300
+build/X86/gem5.opt --outdir=hbm_results/hbm_12_1000_RO_Miss/1024 traffGen.py hbm_1000 16MiB 1024 linear 1000000000000 128MiB 1000 100 nvm_300
