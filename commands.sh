490eab0b0642778d8a5324c5aa0cfc109f4aa530
mbabaie <43761552+mbabaie@users.noreply.github.com>
2021-12-09 13:36:23 -0800

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,119 @@
+#################### SLOW #######################
+
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/1 traffGen.py DDR4_2400_16x4 16MiB 1 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/2 traffGen.py DDR4_2400_16x4 16MiB 2 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/4 traffGen.py DDR4_2400_16x4 16MiB 4 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/8 traffGen.py DDR4_2400_16x4 16MiB 8 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/16 traffGen.py DDR4_2400_16x4 16MiB 16 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/32 traffGen.py DDR4_2400_16x4 16MiB 32 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/64 traffGen.py DDR4_2400_16x4 16MiB 64 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/128 traffGen.py DDR4_2400_16x4 16MiB 128 linear 1000000000000 128MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/256 traffGen.py DDR4_2400_16x4 16MiB 256 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/512 traffGen.py DDR4_2400_16x4 16MiB 512 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_slow/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 linear 1000000000000 128MiB 1000 0 &
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 20MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 20MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 20MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 20MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 20MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 20MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 20MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 20MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 20MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 20MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_slow/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 20MiB 1000 0 &
+
+
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 20MiB 1000 70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_slow/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 20MiB 1000 70 &
+
+
+
+#################### FAST ############################
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/1 traffGen.py DDR4_2400_16x4 16MiB 1 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/2 traffGen.py DDR4_2400_16x4 16MiB 2 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/4 traffGen.py DDR4_2400_16x4 16MiB 4 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/8 traffGen.py DDR4_2400_16x4 16MiB 8 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/16 traffGen.py DDR4_2400_16x4 16MiB 16 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/32 traffGen.py DDR4_2400_16x4 16MiB 32 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/64 traffGen.py DDR4_2400_16x4 16MiB 64 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/128 traffGen.py DDR4_2400_16x4 16MiB 128 linear 1000000000000 128MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/256 traffGen.py DDR4_2400_16x4 16MiB 256 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/512 traffGen.py DDR4_2400_16x4 16MiB 512 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_fast/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 linear 1000000000000 128MiB 1000 0 &
+
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 64MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_fast/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 64MiB 1000 0 &
+
+
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 20MiB 1000 70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_fast/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 20MiB 1000 70 &
+
+######################### BASELINE #############################
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/1 traffGen.py DDR4_2400_16x4 16MiB 1 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/2 traffGen.py DDR4_2400_16x4 16MiB 2 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/4 traffGen.py DDR4_2400_16x4 16MiB 4 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/8 traffGen.py DDR4_2400_16x4 16MiB 8 linear 1000000000000 128MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/16 traffGen.py DDR4_2400_16x4 16MiB 16 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/32 traffGen.py DDR4_2400_16x4 16MiB 32 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/64 traffGen.py DDR4_2400_16x4 16MiB 64 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/128 traffGen.py DDR4_2400_16x4 16MiB 128 linear 1000000000000 128MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/256 traffGen.py DDR4_2400_16x4 16MiB 256 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/512 traffGen.py DDR4_2400_16x4 16MiB 512 linear 1000000000000 128MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss100_base/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 linear 1000000000000 128MiB 1000 0 &
+
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 64MiB 1000     0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 64MiB 1000 0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 64MiB 1000   0 &
+# build/NULL/gem5.opt --outdir=../ddr4_WO_Miss25_base/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 64MiB 1000 0 &
+
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/1 traffGen.py DDR4_2400_16x4 16MiB 1 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/2 traffGen.py DDR4_2400_16x4 16MiB 2 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/4 traffGen.py DDR4_2400_16x4 16MiB 4 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/8 traffGen.py DDR4_2400_16x4 16MiB 8 random 1000000000000 20MiB 1000     70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/16 traffGen.py DDR4_2400_16x4 16MiB 16 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/32 traffGen.py DDR4_2400_16x4 16MiB 32 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/64 traffGen.py DDR4_2400_16x4 16MiB 64 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/128 traffGen.py DDR4_2400_16x4 16MiB 128 random 1000000000000 20MiB 1000 70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/256 traffGen.py DDR4_2400_16x4 16MiB 256 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/512 traffGen.py DDR4_2400_16x4 16MiB 512 random 1000000000000 20MiB 1000   70 &
+# build/NULL/gem5.opt --outdir=../ddr4_R70_Miss25_base/1024 traffGen.py DDR4_2400_16x4 16MiB 1024 random 1000000000000 20MiB 1000 70 &
