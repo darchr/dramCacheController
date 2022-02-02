eb853b64b4435539d960d50f821f20ab9ef7b665
Ayaz Akram <yazakram@ucdavis.edu>
2022-02-02 14:25:25 -0800

    configs: add config scripts to test hbm components

---
+++ configs-mem-test/mem_info.py
@@ -0,0 +1,26 @@
+
+from gem5.components.memory import DualChannelDDR4_2400
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.memory import SingleChannelDDR4_2400
+from gem5.components.memory import SingleChannelLPDDR3_1600
+from gem5.components.memory import SingleChannelHBM
+from gem5.components.memory import SingleChannelHBM_DDR3
+from gem5.components.memory import SingleChannelHBMPseudo
+from gem5.components.memory import HBM2Stack
+from gem5.components.memory import HBM2Stackv2
+
+def GetMemClass(simulator, mem_type):
+    return MemInfoDict[simulator][mem_type]
+
+MemInfoDict = {
+    'gem5' : {
+        'ddr3' : SingleChannelDDR3_1600,
+        'ddr4' : SingleChannelDDR4_2400,
+        'lpddr3' : SingleChannelLPDDR3_1600,
+        'hbm' : SingleChannelHBM,
+        'hbm_pseudo' : SingleChannelHBMPseudo,
+        'hbm_from_ddr3' : SingleChannelHBM_DDR3,
+        'hbm_stack' : HBM2Stack,
+        'hbm_stack_v2' : HBM2Stackv2
+    }
+}
