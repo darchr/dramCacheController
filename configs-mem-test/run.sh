551894f53e423a71e91f1436954f30d8cd00b5bc
mbabaie <mbabaie@ucdavis.edu>
2022-02-07 08:47:27 -0800

    mem: adding run script and plot files
    Change-Id: I9320ee236f38785361d0be1642d2517e0bc28a5c

---
+++ configs-mem-test/run.sh
@@ -0,0 +1,26 @@
+# 
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3/RO_Lin      run_memory_test.py    hbm_from_ddr3   gem5  linear   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3/WO_Lin      run_memory_test.py    hbm_from_ddr3   gem5  linear   0.1   256    0 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3/RO_Ran      run_memory_test.py    hbm_from_ddr3   gem5  random   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3/WO_Ran      run_memory_test.py    hbm_from_ddr3   gem5  random   0.1   256    0 &
+# 
+# 
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stack/RO_Lin      run_memory_test.py    hbm_stack   gem5  linear   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stack/WO_Lin      run_memory_test.py    hbm_stack   gem5  linear   0.1   256    0 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stack/RO_Ran      run_memory_test.py    hbm_stack   gem5  random   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stack/WO_Ran      run_memory_test.py    hbm_stack   gem5  random   0.1   256    0 &
+# 
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv2/RO_Lin      run_memory_test.py    hbm_stack_v2   gem5  linear   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv2/WO_Lin      run_memory_test.py    hbm_stack_v2   gem5  linear   0.1   256    0 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv2/RO_Ran      run_memory_test.py    hbm_stack_v2   gem5  random   0.1   256    100 &
+# ../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv2/WO_Ran      run_memory_test.py    hbm_stack_v2   gem5  random   0.1   256    0 &
+
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv3/RO_Lin      run_memory_test.py    hbm_stack_v3   gem5  linear   0.1   256    100 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv3/WO_Lin      run_memory_test.py    hbm_stack_v3   gem5  linear   0.1   256    0 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv3/RO_Ran      run_memory_test.py    hbm_stack_v3   gem5  random   0.1   256    100 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/HBM2Stackv3/WO_Ran      run_memory_test.py    hbm_stack_v3   gem5  random   0.1   256    0 &
+
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3_v2/RO_Lin      run_memory_test.py    hbm_from_ddr3_v2   gem5  linear   0.1   256    100 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3_v2/WO_Lin      run_memory_test.py    hbm_from_ddr3_v2   gem5  linear   0.1   256    0 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3_v2/RO_Ran      run_memory_test.py    hbm_from_ddr3_v2   gem5  random   0.1   256    100 &
+../build/NULL/gem5.opt     --outdir=../../bufferSize1024_2/SingleChannelHBM_DDR3_v2/WO_Ran      run_memory_test.py    hbm_from_ddr3_v2   gem5  random   0.1   256    0 &
